ac3ed30245a14d092f549f20527033974cb7e3c0
BennEntterprise <kyle@bennentterprise.com>
2020-01-21 23:15:23 -0500

    practice failure of tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 // Respond with 'hello world'
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.send('hello world, this should fail');
 })
 
 // Listen on port 3000
